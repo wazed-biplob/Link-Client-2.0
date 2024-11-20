a14bb19d661ee14885b4bc651803d4e858cc4b05
wazed biplob <www.biplob@gmail.com>
2024-11-20 17:42:11 +0600

    router bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
+
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/login");
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
   return (
     <>
       <div className="border w-10 h-10"></div>
