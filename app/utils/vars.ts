c1fa7fea13a8ba37d87e583125cb895dec431ae3
wazed biplob <www.biplob@gmail.com>
2024-12-01 19:03:41 +0600

    EsLint Rules Fixed

--- app/utils/vars.ts
+++ app/utils/vars.ts
@@ -1,5 +1,4 @@
 const production = true;
-export let API_URL: string;
-production
-  ? (API_URL = "https://link-server-topaz.vercel.app/api/v1/")
-  : (API_URL = "http://localhost:5000/api/v1/");
+export const API_URL: string = production
+  ? "https://link-server-topaz.vercel.app/api/v1/"
+  : "http://localhost:5000/api/v1/";
