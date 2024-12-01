9901bac52ee7c230ae2fb77ad692e89ea23a3319
wazed biplob <www.biplob@gmail.com>
2024-12-01 18:57:22 +0600

    server bugs fixed

--- app/utils/vars.ts
+++ app/utils/vars.ts
@@ -1,5 +1,5 @@
 const production = true;
 export let API_URL: string;
 production
-  ? (API_URL = "https://link-server-beta.vercel.app/api/v1/")
+  ? (API_URL = "https://link-server-topaz.vercel.app/api/v1/")
   : (API_URL = "http://localhost:5000/api/v1/");
