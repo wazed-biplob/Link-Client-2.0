cb2803c5dc6124f6de825bfba1a8134a24d9a702
wazed biplob <www.biplob@gmail.com>
2024-12-28 21:59:28 +0600

    login ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,35 +4,41 @@ import { FormEvent, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setEmail, setImgURL, setUserId } from "../redux/userSlice";
 import { useRouter } from "next/navigation";
-// import Link from "next/link";
+import Link from "next/link";
 import { useLoginMutation } from "../redux/authApi";
 import { IUserLogin } from "../interface/type";
 import { API_URL } from "../utils/vars";
 
 const LoginPage = () => {
-  const [dbStatus, setDbStatus] = useState("Checking...");
-
+  const [dbStatus, setDbStatus] = useState<string>("Checking...");
+  const [dbConnection, setDbConnection] = useState<boolean>(false);
+  const [loginSuccess, setLoginSuccess] = useState<boolean>(false);
+  console.log(dbConnection);
   const router = useRouter();
   const dispatch = useDispatch();
+
   const [formData, setFormData] = useState<IUserLogin>({
     email: "wazed@gmail.com",
     password: "AsusX550LNi5#2.4",
   });
-  const [loginSuccess, setLoginSuccess] = useState(false);
+
   const [fetchFormData, { isLoading, data }] = useLoginMutation();
 
   // database connection check
   useEffect(() => {
     const fetchDbStatus = async () => {
       try {
-        const res = await fetch(`${API_URL}db/db-status`);
+        const res = await fetch(`${API_URL}/db/db-status`);
         const data = await res.json();
         setDbStatus(data?.message);
         if (data?.status === "success") {
           clearInterval(interval);
+          setDbConnection(true);
         }
       } catch (err) {
-        setDbStatus("Error Connecting Database. Error : " + err);
+        setDbStatus("Error : " + err);
+        clearInterval(interval);
+        setDbConnection(false);
       }
     };
 
@@ -51,12 +57,12 @@ const LoginPage = () => {
   // login handler
   const onSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    const res = await fetchFormData(formData);
-    if (res?.data?.success === true) {
-      const id = res?.data?.data?._id;
-      dispatch(setUserId(res?.data?.data?._id));
-      dispatch(setImgURL(res?.data?.data?.imgURL));
-      dispatch(setEmail(res?.data?.data?.email));
+    const res = await fetchFormData(formData).unwrap();
+    if (res?.success === true) {
+      const id = res?.data?._id;
+      dispatch(setUserId(res?.data?._id));
+      dispatch(setImgURL(res?.data?.imgURL));
+      dispatch(setEmail(res?.data?.email));
       router.push(`${id}`);
     }
   };
@@ -72,9 +78,9 @@ const LoginPage = () => {
             </h1>
           </div>
           <div className="text-center">
-            {/* <p className="text-sm my-4 text-gray-600">
+            <p className="text-sm my-4 text-gray-600">
               Sign in to access your account
-            </p> */}
+            </p>
           </div>
           <form onSubmit={onSubmit}>
             <div className="space-y-4">
@@ -100,12 +106,13 @@ const LoginPage = () => {
                   className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
                 />
 
-                {/* <Link
+                <Link
+                  onClick={() => alert("not implemented")}
                   href="#"
                   className="text-xs mt-1 mb-3 text-right hover:underline text-gray-600"
                 >
                   Forgot password?
-                </Link> */}
+                </Link>
               </div>
             </div>
             <div>
@@ -117,30 +124,30 @@ const LoginPage = () => {
                     isLoading || loginSuccess
                       ? "bg-slate-500 cursor-not-allowed"
                       : "bg-violet-600 hover:bg-violet-700"
-                  } w-full border mt-2 px-8 py-3 font-semibold rounded-md  text-gray-50 `}
+                  } w-full border mt-2 px-8 py-3 font-semibold rounded-md text-gray-50 `}
                 >
-                  {!(isLoading || loginSuccess) && <span>Log in</span>}
+                  {!(isLoading || loginSuccess) && <span>Sign In</span>}
                   {isLoading && !loginSuccess && (
                     <span className="text-[16px]">&nbsp;...Loading</span>
                   )}
-                  {loginSuccess && (
+                  {!isLoading && loginSuccess && (
                     <span className="text-[16px]">&nbsp;...Redirecting</span>
                   )}
                 </button>
               </div>
-              {/* <p className="px-6 mt-2 text-sm text-center text-gray-600">
-                Don&apos;t have an account yet?
+              <p className="px-6 mt-2 text-sm text-center">
                 <Link
-                  href="/register"
+                  onClick={() => alert("not implemented")}
+                  href="#"
                   className="hover:underline text-violet-600"
                 >
-               
+                  Don&apos;t have an account yet?
                 </Link>
-              </p> */}
+              </p>
             </div>
           </form>
           {
-            <span className="text-[16px] mt-2 border rounded-sm px-2">
+            <span className="text-[12px] mt-2 border rounded-sm px-2">
               {dbStatus}
             </span>
           }
